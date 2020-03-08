e805f01d45e83615f323860c5bff7fb75871c7b3
Sarthak Sharma <sarthaksharma2199@outlook.com>
2020-03-08 21:45:31 +0530

    First Commit

---
+++ week5/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sarthak Sharma PHP</title>
+</head>
+<body>
+
+<H1>Sarthak Sharma PHP</H1>
+    <pre>
+        ******
+        *
+        *
+        ******
+             *
+             *
+        ******     
+    
+    </pre>
+
+    <?php
+    echo "\n The SHA is :\n";
+    print hash('sha256', 'Sarthak Sharma'); 
+
+    ?>
+<br>
+<a href="check.php">Click here to check error setting</a>
+<br>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
+</html>
